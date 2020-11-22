f5b3c0f8c0c435592c9a66c60219f47ca13d0691
evgueni.tsygankov <evgueni.tsygankov@refinitiv.com>
2020-11-21 23:48:26 -0600

    Add project files.

---
+++ TestWebApi/Controllers/Service1Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TestWebApi.Controllers
+{
+    public class Service1Controller : ApiController
+    {
+        [HttpGet]
+        async public Task<string> ExecuteMethod()
+        {
+            await Task.Delay(250);
+            return "Done";
+        }
+    }
+}
---
+++ TestWebApiApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestWebApiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var threads = new List<Thread>();
+            for (var i = 0; i < 100; i++)
+            {
+                var thread = new Thread(() =>
+                {
+                    var time = DateTime.Now;
+                    var line = $"Starting thread {i} at {time}. ";
+                    var result = client.GetStringAsync("http://localhost:2614/api/service1").Result;
+                    line += $"Done in {DateTime.Now.Subtract(time).TotalMilliseconds} ms";
+                    Console.WriteLine(line);
+                });
+                thread.Start();
+                threads.Add(thread);
+            }
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+            Console.WriteLine("Done");
+            Console.Read();
+        }
+    }
+}
